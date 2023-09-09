1cb10f9deea47124a0adc3743faa21507c94eadb
Fiorella <124637724+fiofiorito@users.noreply.github.com>
2023-09-09 17:19:18 -0300

    add useState on navbar, so it is now responsive on mobile, change e-commerce name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,53 @@
+import { useState } from "react";
 import CartWidget from "./CartWidget";
 import "./NavBar.css"
 
 function NavBar() {
+    // change className --> toggle navbar
+    const [hamburgerLine, setHamburgerLine] = useState("line unclicked");
+    const [navBar, setNavBar] = useState("nav-bar hidden");
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+    const updateMenu = () => {
+        if (!isMenuClicked) {
+            setHamburgerLine("line clicked");
+            setNavBar("nav-bar visible");
+            console.log(hamburgerLine, navBar);
+        } else {
+            setHamburgerLine("line unclicked");
+            setNavBar("nav-bar hidden");
+        }
+        setIsMenuClicked(!isMenuClicked);
+    }
+
+
+
     return <>
-        <div class="header-container">
-            <div class="logo">iFio :&#41;</div>
-            <div class="hamburger">
-                <div class="line"></div>
-                <div class="line"></div>
-                <div class="line"></div>
+        <div className="header-container">
+            <div className="logo">iFit Gym</div>
+            <div className="hamburger" onClick={updateMenu}>
+                <div className={hamburgerLine}></div>
+                <div className={hamburgerLine}></div>
+                <div className={hamburgerLine}></div>
             </div>
-            <nav class="nav-bar">
-                <ul class="nav-bar-list">
-                    <li class="nav-bar-item">
-                        <a class="nav-bar-link" href="#">Inicio</a>
+            <nav className={navBar}>
+                <ul className="nav-bar-list">
+                    <li className="nav-bar-item">
+                        <a className="nav-bar-link" href="#">Inicio</a>
                     </li>
-                    <li class="nav-bar-item">
-                        <a class="nav-bar-link" href="#">Promociones</a>
+                    <li className="nav-bar-item">
+                        <a className="nav-bar-link" href="#">Promociones</a>
                     </li>
-                    <li class="nav-bar-item">
-                        <a class="nav-bar-link" href="#">Servicios</a>
+                    <li className="nav-bar-item">
+                        <a className="nav-bar-link" href="#">Servicios</a>
                     </li>
-                    <li class="nav-bar-item">
-                        <a class="nav-bar-link" href="#">Soporte</a>
+                    <li className="nav-bar-item">
+                        <a className="nav-bar-link" href="#">Soporte</a>
                     </li>
                 </ul>
             </nav>
-            <a class="nav-bar-link nav-bar-cart-widget-link" href="#"><CartWidget /></a>
+            <a className="nav-bar-link nav-bar-cart-widget-link" href="#"><CartWidget /></a>
         </div>
-        <div class="solid-line"></div>
+        <div className="solid-line"></div>
     </>
 }
 
