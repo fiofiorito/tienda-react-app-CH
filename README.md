b2ddef37723f4fe7d639eba302d32f5866e89437
Fiorella <124637724+fiofiorito@users.noreply.github.com>
2023-09-24 22:46:40 -0300

    add gif

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Pagina de E-commerce iFit Gym
+Segunda entrega: gif demostrando las rutas.
+Nota: La ruta "/category/:categoryId" no se encuentra seteada porque no filtre a los productos por categoria todavia, pero lo voy a hacer :&#41;
+
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Items from "./Items";
 
 const ItemList = ({ items }) => {
+
     return <div className="grid">
         {items.map((item) => {
             return <Items key={item.id} item={item} />
